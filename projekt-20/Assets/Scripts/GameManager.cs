d2090b091d513108f8bca3d5f9d30c608170b344
Frantisek Kopecky <frantisekkopecky29@gmail.com>
2025-12-09 11:15:30 +0100

    Added SaveSystem

--- projekt-20/Assets/Scripts/GameManager.cs
+++ projekt-20/Assets/Scripts/GameManager.cs
@@ -1,12 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    void Start()
+    private static string savePath = Application.persistentDataPath + "/SaveGame.json";
+    public Vector3 PlayerData()
     {
         
+        return transform.position;
     }
+
+    public static void Savegame()
+    {
+        CheckExistence();
+        
+        
+        string json = JsonUtility.ToJson(data,true);
+        File.WriteAllText(savePath, json);
+        
+    }
+
+    public static void Loadgame()
+    {
+        CheckExistence();
+        
+        string json = File.ReadAllText(savePath);
+        
+    }
+
+    public static void CheckExistence()
+    {
+        if (!File.Exists(savePath))
+        {
+            File.WriteAllLines(savePath, new string[] { });
+        }
+    }
+    
+    
+    
+    
 }
 
--- projekt-20/Assets/Scripts/MainMenu.cs
+++ projekt-20/Assets/Scripts/MainMenu.cs
@@ -13,35 +13,5 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quit");
-    }
-    public SaveSystem saveSystem; 
-
-    public void ContinueGame()
-    {
-        // Pokud existuje ulo�en� hra, na�ti pozici hr��e
-        if (PlayerPrefs.HasKey("HasSave") && PlayerPrefs.GetInt("HasSave") == 1)
-        {
-            // Na��t�n� pozice hr��e
-            if (saveSystem != null)
-            {
-                //saveSystem.LoadGame(); // Na��st pozici
-                SceneManager.LoadScene("SampleScene"); // P�echod do hern� sc�ny (GameScene)
-            }
-            else
-            {
-                Debug.LogError("SaveSystem nen� p�ipojen. Ujist�te se, �e je spr�vn� propojen.");
-            }
-        }
-        else
-        {
-            Debug.LogError("��dn� ulo�en� hra nen� k dispozici.");
-        }
-    }
-
-    public void NewGame()
-    {
-        // Pokud za��n�te novou hru, sma�te ulo�en� data
-        PlayerPrefs.DeleteAll();
-        SceneManager.LoadScene("SampleScene"); // P�echod na novou hru
-    }
+    } 
 }
